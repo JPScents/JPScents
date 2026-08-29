40183a603e4cb9b77eb696f5dc11f1f61c79314e
Freeman-md <66797587+Freeman-md@users.noreply.github.com>
2026-08-29 09:49:01 +0100

    refactor: remove obsolete page bridges

--- src/app/(public)/checkout/Checkout.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { Checkout as OrderCheckout } from "@/features/orders/client";
-import { useCart } from "@/features/cart";
-
-export function Checkout() {
-  return <OrderCheckout cart={useCart()} />;
-}
